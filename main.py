37856a903bd1a62aedfd7604b59f94232fccd899
ilan1031 <ilanthalirs10@gmail.com>
2025-11-18 12:44:57 +0000

    inital commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+
+from rag_system import load_and_split_documents, create_vector_store, setup_rag_chain
+
+#Initialize the API
+app = FastAPI()
+
+class Query(BaseModel):
+    question: str
+
+try:
+    FILE_PATH = "knowladge.txt"
+    if not os.path.esists(FILE_PATH):
+        with open(FILE_PATH, "W") as f:
+            f.write("FastAPI")
+    chunks = load_and_split_documents(FILE_PATH)
+    vector_store = create_vector_store(vector_store)
+except Exception as e:
+    raise RuntimeError(f"Failed to initialize RAG : {e}")  
+
+@app.post("/ask")
+async def ask_question(query: Query):
+    try:
+        result = qa_chain.invoke({"query": query.question})
+        return {"response": result["result"]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))                      
+
+
---
+++ rag_system.py
@@ -0,0 +1,44 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.community.embeddings import SentenceTransformerEmbeddings
+from langchain.community.vectorstores import Chroma
+from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
+
+import os
+
+def load_and _split_documents(file_path):
+    #load .txt
+    loader = TextLoader(file_path)
+    documents = loader.load()
+
+    #split the doc
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter._split_documents(documents)
+    return docs
+
+# use 
+file_path = "knowladge.txt"
+chunks = load_and_split_documents(file_path)
+
+
+def create_vector_store(chunks)
+    # Create embeddings
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # Create vector store
+    create_vector_store = Chroma.from_documents(documents=chunks, embeddings=embeddings)
+    return create_vector_store
+
+create_vector_store = create_vector_store(chunks)    
+
+
+def setup_rag_chain(create_vector_store):
+    llm = Ollama(model="llama2")
+    retriever = vector_store.as_retriever()
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        chain_type="stuff",
+        retriever=retriever
+    )
+    return qa_chain
